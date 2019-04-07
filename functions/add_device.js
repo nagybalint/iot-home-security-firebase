5a761c06c153f6d3920ffb773269ef1c296a03cb
nagybalint <nagybalint000@gmail.com>
2019-04-08 01:07:31 +0200

    Add function skeletons

--- functions/add_device.js
+++ functions/add_device.js
@@ -1,8 +1,29 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-module.exports = async (uid, device_id) => {
-    return {
-        test: "Test"
-    };
+module.exports = async (data, context) => {
+    // Only authenticated users can user this endpoint
+    if(!context.auth) {
+        console.log(`Unauthenticated function call`);
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'You must be authenticated to call this endpoint'
+        );
+    }
+
+    const { uid } = context.auth;
+
+    console.log(`Authenticated call with id ${uid}`);
+
+    try {
+        let userRef = await admin.firestore().collection('users').doc(uid);
+        // TODO
+        // Functionality comes here
+
+    } catch (error) {
+        throw new functions.https.HttpsError(
+            'internal',
+            'Something bad has happened :( Please try again!'
+        );
+    }
 }
